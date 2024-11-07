714afb5cb1aab74001182e02160e9fd7a1bdd40f
Chavindu Jayakody <chavindujayakody2001@gmail.com>
2024-11-07 23:40:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 
 include('dbconnection.php');
 
-function sanitizeInput($input) {
+function sanitizeInput($input)
+{
     $input = trim($input);
     $input = stripslashes($input);
     $input = htmlspecialchars($input);
@@ -75,6 +76,7 @@ mysqli_close($conn);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -82,193 +84,139 @@ mysqli_close($conn);
     <link rel="stylesheet" href="style.css">
 
     <link rel="stylesheet"
-    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+        href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    
 
-    
+
+
 </head>
+
 <body>
 
-  <!-- header -->
-<section>
-    <header>
-        <a href="#" class="logo">
-        <img src="Images/logo (1).png">
-        </a>
+    <!-- header -->
+    <?php
+    include 'components/header.php';
+    ?>
 
-        <ul class="navlist">
-            <li><a href="index.html" >Home</a></li>
-            <li><a href="menu.php">Menu</a></li>
-            <li><a href="reservations.php">Reservations</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#footer">Contact Us</a></li>
-        </ul>
+    <!-- Login -->
 
-        <div class="nav-icons">
-            <a href="#"><i class="bx bx-cart"></i></a>
-            <a href="#login-form"><i class="bx bx-user userbutton " id="user"></i></a>
-            <div class="bx bx-menu" id="menu-icon"></div>
-        </div>
-    </header> 
-</section>
+    <section class="container forms">
+        <div class="form-overlay show-login">
+            <div class="form login">
+                <div class="form-content login-form">
+                    <h1>Login</h1>
 
-<!-- Login -->
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                        <div class="field input-field">
+                            <input type="email" placeholder="Email" class="input" name="email">
+                        </div>
 
-<section class="container forms">
-    <div class="form-overlay show-login">
-        <div class="form login">
-            <div class="form-content login-form">
-                <h1>Login</h1>
+                        <div class="field input-field">
+                            <input type="password" placeholder="Password" class="password" name="password">
+                            <i class="bx bx-hide eye-icon"></i>
+                        </div>
 
-                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">                    
-                <div class="field input-field">
-                        <input type="email" placeholder="Email" class="input" name="email">
-                    </div>
+                        <div class="form-link">
+                            <a href="#" class="forgot-pass">Forgot Password?</a>
+                        </div>
 
-                    <div class="field input-field">
-                        <input type="password" placeholder="Password" class="password" name="password">
-                        <i class="bx bx-hide eye-icon"></i>
-                    </div>
+                        <div class="field button-field">
+                            <button type="submit" name="login">Login</button>
+                        </div>
 
+
+                    </form>
                     <div class="form-link">
-                    <a href="#" class="forgot-pass">Forgot Password?</a>
-                    </div>
-
-                    <div class="field button-field">
-                        <button type="submit" name="login">Login</button> 
-                    </div>
-
-
-                </form>
-                <div class="form-link">
                         <span>Already have an account? <a href="#" class="signup-link">Signup</a></span>
                     </div>
+                </div>
+
+                <div class="line"> </div>
+
             </div>
 
-            <div class="line"> </div>
+            <!-- Signup  -->
 
-        </div>
+            <div class="form signup">
+                <div class="form-content signup-form">
+                    <h1>Signup</h1>
 
-<!-- Signup  -->
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                        <div class="field input-field">
+                            <input type="text" placeholder="Full Name" class="input" name="fullName">
+                        </div>
 
-        <div class="form signup">
-            <div class="form-content signup-form">
-                <h1>Signup</h1>
+                        <div class="field input-field">
+                            <input type="email" placeholder="Email" class="input" name="email">
+                        </div>
 
-          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">                    
-          <div class="field input-field">
-                        <input type="text" placeholder="Full Name" class="input" name="fullName">
-                    </div>
+                        <div class="field input-field">
+                            <input type="password" placeholder="Password" class="password" name="password">
+                        </div>
 
-                    <div class="field input-field">
-                        <input type="email" placeholder="Email" class="input" name="email">
-                    </div>
+                        <div class="field input-field">
+                            <input type="password" placeholder="Password" class="password" name="password">
+                            <i class="bx bx-hide eye-icon"></i>
+                        </div>
 
-                    <div class="field input-field">
-                        <input type="password" placeholder="Password" class="password" name="password">
-                    </div>
+                        <div class="field button-field">
+                            <button type="submit" name="signup">Signup</button>
+                        </div>
 
-                    <div class="field input-field">
-                        <input type="password" placeholder="Password" class="password" name="password">
-                        <i class="bx bx-hide eye-icon"></i>
-                    </div>
-
-                    <div class="field button-field">
-                        <button type="submit" name="signup">Signup</button> 
-                    </div>
-
-                </form>
-                <div class="form-link">
+                    </form>
+                    <div class="form-link">
                         <span>Already have an account? <a href="#" class="login-link">Login</a></span>
                     </div>
+                </div>
+
+                <div class="line"> </div>
+
             </div>
-
-            <div class="line"> </div>
-
         </div>
-    </div>
-</section>
+    </section>
 
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const loginLink = document.querySelector('.login-link');
-        const signupLink = document.querySelector('.signup-link');
-        const formsContainer = document.querySelector('.forms');
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const loginLink = document.querySelector('.login-link');
+            const signupLink = document.querySelector('.signup-link');
+            const formsContainer = document.querySelector('.forms');
 
-        loginLink.addEventListener('click', function (e) {
-            e.preventDefault();
-            formsContainer.classList.remove('show-signup');
-            formsContainer.classList.add('show-login');
+            loginLink.addEventListener('click', function(e) {
+                e.preventDefault();
+                formsContainer.classList.remove('show-signup');
+                formsContainer.classList.add('show-login');
+            });
+
+            signupLink.addEventListener('click', function(e) {
+                e.preventDefault();
+                formsContainer.classList.remove('show-login');
+                formsContainer.classList.add('show-signup');
+            });
         });
-
-        signupLink.addEventListener('click', function (e) {
-            e.preventDefault();
-            formsContainer.classList.remove('show-login');
-            formsContainer.classList.add('show-signup');
-        });
-    });
-</script>
+    </script>
 
 
-<!-- Footer -->
-<footer id="footer">
-    <div class="footer-content">
-        <div class="col">
-            <img src="Images/logo.png" alt="logo" class="footer-logo">
-            <p>Savor the Moment, Taste the Difference – Outer Clove,<br> Where Culinary Excellence Meets Casual Elegance.</p>
-        </div>
-        <div class="col">
-            <h3>Office <div class="underline"><span></span></div></h3>
-            <p>123 Flower Road, Piliyandala</p>
-            <p class="email-id">contact@outerclove.com</p>
-            <h4>+94 777 9696 45</h4>
-        </div>
-        <div class="col">
-            <h3>Branch 2 <div class="underline"><span></span></div></h3>
-            <p>456 New York City</p>
-            <p class="email-id">contact@outerclovebranch2.com</p>
-            <h4>+1 123 456 7890</h4>
-        </div>
-        <div class="col">
-            <ul class="footer-list">
-                <h3>Links <div class="underline"><span></span></div></h3>
-                <li class="active"><a href="#banner">Home</a></li>
-                <li><a href="#menu">Menu</a></li>
-                <li><a href="reservation.html">Reservations</a></li>
-                <li><a href="#offers">About Us</a></li>
-                <li><a href="#contact">Contact Us</a></li>
-            </ul>
-        </div>
-     
-        <div class="social">
-            <a href="#" class="clr"><i class="bx bxl-instagram-alt"></i></a>
-            <a href="#"> <i class="bx bxl-facebook"></i></a>
-            <a href="#"><i class="bx bxl-twitter"></i></a>
-            <a href="#"><i class="bx bxl-github"></i></a>
-            <a href="#"><i class="bx bxl-google"></i></a>
-        </div>
-</div>
-    <hr>
-    <p class="copyright">© Chavindu Jayakody 2023- All Rights Reserved</p>
-</footer>
+    <!-- Footer -->
+    <?php
+    include 'components/footer.php';
+    ?>
 
-<!--Scroll Top  -->
-<a href="#" class="scroll">
-    <i class="bx bx-up-arrow-alt"></i>
-</a>
+    <!--Scroll Top  -->
+    <a href="#" class="scroll">
+        <i class="bx bx-up-arrow-alt"></i>
+    </a>
 
 
 
-<script src="js/script.js"></script>
+    <script src="js/script.js"></script>
 
-<script src="https://unpkg.com/scrollreveal"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
 
 
 </body>
-</html>
 
+</html>
