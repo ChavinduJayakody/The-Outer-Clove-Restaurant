cd828f32d7b084867537672faa4cc05e7854eac5
Chavindu Jayakody <chavindujayakody2001@gmail.com>
2024-11-07 23:32:48 +0530

    redesigned about page

---
+++ about.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Outer Clove - About Us</title>
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <?php
+    include 'components/header.php';
+    ?>
+
+    <!-------------------About Us--------------------------------------------------------------->
+    <section id="about-mid">
+        <div class="content-middle ">
+            <div class="detailsofus">
+                <br>
+                <h2>Who we are</h2>
+                <br>
+                <br>
+                <p><i class="fa fa fa-quote-left"></i>
+                    Nestled in the heart of Colombo, The Outer Clove is a culinary haven that epitomizes a harmonious
+                    blend of taste, ambiance, and hospitality.
+                    Our restaurant is more than just a dining destination; it's an immersive experience crafted to
+                    delight the senses.
+                    From the vibrant tapestry of flavors in our meticulously curated menu to the inviting ambiance that
+                    exudes warmth,
+                    every visit to The Outer Clove is a celebration. Our commitment to quality ingredients, innovative
+                    recipes,
+                    and exceptional service ensures that each guest enjoys not just a meal but an unforgettable journey
+                    through taste and hospitality.
+
+                    <i class="fa fa fa-quote-right"></i>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!------------------Our Services--------------------------------------------------------------->
+
+    <section class="services">
+        <div class="container-services">
+            <div class="section-header">
+                <h2>OUR SERVICES</h2>
+
+
+                <div class="service-items">
+                    <div class="service-item">
+                        <img src="images/service1.jpg" alt="Service 1">
+                        <h3>Dine-In</h3>
+                        <p>"Dining Perfected, Every Moment Treasured."</p>
+                    </div>
+                    <div class="service-item">
+                        <img src="images/service2.jpg" alt="Service 2">
+                        <h3>Take Away</h3>
+                        <p>"Take it Home, Taste the Difference."</p>
+                    </div>
+                    <div class="service-item">
+                        <img src="images/service3.jpg" alt="Service 2">
+                        <h3>Delivery</h3>
+                        <p>"Delivering Delightful Flavors to Your Doorstep."</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!------------------Our Availabilities--------------------------------------------------------------->
+
+    <section class="availability">
+        <div class="availability-container">
+            <div class="availability-section-header">
+                <h2>RESTAURANT FACILITIES</h2>
+
+            </div>
+            <div class="availabilityy">
+                <div class="available-am">
+                    <img src="images/wifi.jpeg" alt="availability 1">
+                    <h3>FREE WIFI</h3>
+                    <p>"Dining Perfected, Every Moment Treasured."</p>
+                </div>
+                <div class="available-am">
+                    <img src="images/parking.jpeg" alt="availability 2">
+                    <h3>PARKING</h3>
+                    <p>"Take it Home, Taste the Difference."</p>
+                </div>
+                <div class="available-am">
+                    <img src="images/music.jpg" alt="availability 3">
+                    <h3>LIVE MUSIC</h3>
+                    <p>"Delivering Delightful Flavors to Your Doorstep."</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-------------------------------------Our Team---------------------------------------------->
+
+    <div class="container" style="background-image: url(Images/team.jpg); background-size: cover;background-attachment: fixed;">
+        <div class="abouts">
+            <h1> OUR TEAM</h1>
+
+            <div class="sub-container">
+
+                <div class="team">
+
+                    <img src="images/person1.jpg" alt="">
+                    <div class="name">Daniel Trevor</div>
+                    <div class="desig">CEO</div>
+                    <div class="aboutme"><i class="fa fa fa-quote-left"></i> Daniel Trevor, CEO of The Outer Clove Restaurant, blends culinary expertise
+                        with visionary leadership to create memorable dining experiences. His commitment to exceptional
+                        taste and hospitality defines The Outer Clove's success under his guidance. <i class="fa fa fa-quote-right"></i></div>
+
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+
+                <div class="team">
+
+                    <img src="images/person2.jpeg" alt="">
+                    <div class="name">Kevin Jonas</div>
+                    <div class="desig">MANAGER</div>
+                    <div class="aboutme"><i class="fa fa fa-quote-left"></i>Meet Kevin Jonas, the adept manager of The Outer Clove Restaurant.
+                        With a knack for seamless operations and a passion for customer satisfaction,
+                        Kevin orchestrates the restaurant's daily affairs with precision and warmth.
+                        <i class="fa fa fa-quote-right"></i>
+                    </div>
+
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+
+                <div class="team">
+
+                    <img src="images/person3.jpeg" alt="">
+                    <div class="name">Julia Roberts</div>
+                    <div class="desig">DIRECTOR</div>
+                    <div class="aboutme"><i class="fa fa fa-quote-left"></i>The proficient Director at The Outer Clove,
+                        intertwines creativity and strategy to elevate the restaurant's brand presence.
+                        Her initiatives captivate audiences,bringing the essence of The Outer Clove's culinary
+                        delights to a broader audience.<i class="fa fa fa-quote-right"></i></div>
+
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+<!-- footer -->
+<?php
+include 'components/footer.php';
+?>
+
+</body>
+
+</html>
